d46fac0f319f4c514e1ae2671a4deca2dacf828e
MingliCheng <cml321@hotmail.com>
2019-03-20 08:39:41 +0800

    elemnet-antdesign-init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div>
+      <a-button>ant</a-button>
+      <el-button>elment</el-button>
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
